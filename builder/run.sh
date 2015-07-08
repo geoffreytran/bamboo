c846cb2a5684e9bdbbff2087bca2774362d92e3a
Geoffrey Tran <geoffrey.tran@gmail.com>
2015-07-08 17:58:17 -0500

    Customized error pages for better debugging

--- builder/run.sh
+++ builder/run.sh
@@ -3,5 +3,13 @@ if [[ -n $BAMBOO_DOCKER_AUTO_HOST ]]; then
 sed -i "s/^.*Endpoint\": \"\(http:\/\/haproxy-ip-address:8000\)\".*$/    \"EndPoint\": \"http:\/\/$HOST:8000\",/" \
     ${CONFIG_PATH:=config/production.example.json}
 fi
+
+# Generate configuration by looping through .dist files in /etc/nginx/conf.d/default.conf
+for f in /etc/haproxy/errors/*.html
+do
+    # Replace ${VAR} with variables set in environment.
+    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' $f > $f
+done
+
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
 /usr/bin/supervisord
--- etc/haproxy/errors/README.md
+++ etc/haproxy/errors/README.md
@@ -1,6 +1,10 @@
-NGINX Error Pages
+HAProxy Error Pages
 -------------------
 
 The error pages located in this directory are generated using
 [Better Error Pages](https://better-error-pages.statuspage.io/).
 Use their editor to customize the pages to your project.
+
+Also appended is an HTML comment at the end of error pages to allow easier debugging in a complex environment.
+
+    <!-- Bamboo ${HOSTNAME} on ${HOST} -->
